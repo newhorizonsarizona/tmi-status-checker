5abb0f673116b93fdbf8b9a871b90adccccc3a01
Anand Vijayan <anand.vijayachandran@gmail.com>
2025-05-23 17:49:56 -0700

    Changes to support aws ses and email cc (#3)

--- send_mail/main.go
+++ send_mail/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	notify "github.com/d2tm/tmi-status-checker/util/notify"
 	"gopkg.in/gomail.v2"
@@ -11,20 +12,26 @@ import (
 func main() {
 	notify.LoadDcpReport()
 	m := gomail.NewMessage()
-	email_from := os.Getenv("EMAIL_ID")
+	email_user := os.Getenv("EMAIL_USER")
 	email_password := os.Getenv("EMAIL_PASSWORD")
 	club_number := os.Getenv("CLUB_NUMBER")
 	club_name := os.Getenv("CLUB_NAME")
-	email_to := "members-" + club_number + "@toastmastersclubs.org"
+	email_from := os.Getenv("EMAIL_FROM")
+	email_to := os.Getenv("EMAIL_TO")
+	email_cc := os.Getenv("EMAIL_CC")
 	//email_to := "anand_vijayan@yahoo.com" //TODO: Remove after testing
 	email_subject := notify.GetSummary()
 	email_body := generateMessageBody(club_number, club_name)
 	m.SetHeader("From", email_from)
 	m.SetHeader("To", email_to)
+	cc_addrs := strings.Split(email_cc, ",")
+	if len(cc_addrs) > 0 {
+		m.SetHeader("Cc", cc_addrs...)
+	}
 	m.SetHeader("Subject", "DCP Report: "+email_subject)
 	m.SetBody("text/plain", email_body)
 	m.Attach("../reports/dcp_report.png")
-	d := gomail.NewDialer("smtp.gmail.com", 587, email_from, email_password)
+	d := gomail.NewDialer("email-smtp.us-west-2.amazonaws.com", 587, email_user, email_password)
 
 	if err := d.DialAndSend(m); err != nil {
 		panic(err)
